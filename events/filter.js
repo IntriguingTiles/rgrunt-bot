09f530d2a0fa3e723ae3c184fbd5fc947ea89b1c
IntriguingTiles <jnickw@outlook.com>
2020-06-03 00:49:45 -0600

    Check message updates against the filter

--- events/filter.js
+++ events/filter.js
@@ -9,6 +9,7 @@ let client;
 exports.register = c => {
     client = c;
     c.on("message", message);
+    c.on("messageUpdate", messageUpdate);
 };
 
 /**
@@ -16,6 +17,7 @@ exports.register = c => {
  */
 exports.deregister = c => {
     c.removeListener("message", message);
+    c.removeListener("messageUpdate", messageUpdate);
 };
 
 /**
@@ -36,6 +38,29 @@ async function message(msg) {
         if (msg.content.match(word)) {
             msg.delete();
             msg.badWords = true;
+        }
+    });
+}
+
+/**
+ * @param {Message} oldMsg 
+ * @param {Message} newMsg 
+ */
+async function messageUpdate(oldMsg, newMsg) {
+    if (newMsg.channel.type === "dm") return;
+    if (newMsg.author.bot) return;
+    if (newMsg.content.length === 0) return;
+    if (!newMsg.deletable) return;
+    if (newMsg.member.hasPermission("MANAGE_GUILD")) return;
+
+    const badWords = client.badWords.get(newMsg.guild.id);
+
+    if (badWords.length === 0) return;
+
+    badWords.forEach(word => {
+        if (newMsg.content.match(word)) {
+            newMsg.delete();
+            newMsg.badWords = true;
         }
     });
 }
