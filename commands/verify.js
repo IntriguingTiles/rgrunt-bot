6d44e6f75f4a2a55fd60a4ecb24c0dee194903e7
IntriguingTiles <jnickw@outlook.com>
2026-08-04 14:46:38 -0600

    Commands: Verify: Defer immediately

--- commands/verify.js
+++ commands/verify.js
@@ -28,12 +28,13 @@ exports.run = async (client, intr, guildSettings) => {
     if (!guildSettings.verifyRole) return intr.reply({ content: "Use `/roleconfig verify` before using this command.", ephemeral: true });
     if (!intr.guild.roles.cache.has(guildSettings.verifyRole)) return intr.reply({ content: "The role used for verify no longer exists.", ephemeral: true });
     if (!intr.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageRoles)) return intr.reply({ content: "I don't have permission to manage roles.", ephemeral: true });
+    await intr.deferReply();
 
     try {
         await intr.options.getMember("user").roles.add(guildSettings.verifyRole, `Verified by ${intr.user.tag} (${intr.user.id})`);
-        intr.reply(`Verified ${intr.options.getMember("user")}.`);
+        intr.followUp(`Verified ${intr.options.getMember("user")}.`);
     } catch (err) {
         console.log(err);
-        intr.reply({ content: "Failed to add the verified role!", ephemeral: true });
+        intr.followUp("Failed to add the verified role.");
     }
 };
